f73a347992b620f3061f7fe4f3ada987d31d6830
Darcy LF <ehnevault@gmail.com>
2022-04-21 17:57:38 +1000

    :lipstick: use nonidealstate for homepage currently

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Block } from 'baseui/block';
-import {ParagraphMedium} from 'baseui/typography'
+import {ParagraphMedium} from 'baseui/typography';
+import NonIdealState from '../components/NonIdealState';
+import { Plus } from 'baseui/icon';
+import { Button } from 'baseui/button';
+import { useStyletron } from 'baseui';
+import Link from 'next/link';
 
 const Index = () => {
+  const [css, theme] = useStyletron();
+
   return (
     <Block>
-      <ParagraphMedium>This is where the list of items would go</ParagraphMedium>
+      <NonIdealState
+        Icon={Plus}
+        text="Looks like you haven't added any items yet."
+        action={(
+          <Link href="/addProduct" passHref>
+            <Button 
+              size="compact"
+              shape="pill"
+              $as="a"
+              //colors={{backgroundColor: theme.colors.accent, color: theme.colors.contentOnColor}}
+            >
+              Add new item
+            </Button>
+          </Link>
+          
+        )} 
+      />
     </Block>
   );
 }
