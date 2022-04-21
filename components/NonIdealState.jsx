8888eec148491de538c16135a52ebe5897265719
Darcy LF <ehnevault@gmail.com>
2022-04-21 17:56:44 +1000

    :sparkles: add nonidealstate component

---
+++ components/NonIdealState.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Block } from 'baseui/block';
+import { styled, withStyle } from 'baseui';
+import { ParagraphMedium } from 'baseui/typography';
+
+const ContainerBox = styled(Block, ({$theme}) => ({
+  backgroundColor: $theme.colors.backgroundTertiary,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  width: '100%',
+  textAlign: 'center',
+  padding: $theme.sizing.scale1000,
+  paddingLeft: $theme.sizing.scale1400,
+  paddingRight: $theme.sizing.scale1400,
+
+}))
+
+const Nonidealstate = ({Icon, text, action}) => {
+  return (
+    <ContainerBox>
+      <Icon size={64} color="contentTertiary"/>
+      <ParagraphMedium marginBottom="scale600">
+        {text}
+      </ParagraphMedium>
+      {action}
+    </ContainerBox>
+  );
+}
+
+export default Nonidealstate;
