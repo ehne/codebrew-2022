8b5fb228670cd16fe82cd253a721b40d280aceaa
BariumDiSodium <104109963+BariumDiSodium@users.noreply.github.com>
2022-04-21 18:28:25 +1000

    fixed lukes mistakes 😀
    also accounted for if there isnt a longevity for that storage method

--- api/getExpiry.py
+++ api/getExpiry.py
@@ -1,6 +1,7 @@
 import csv
 import json
-from datetime import *
+import datetime
+from datetime import timedelta
 
 def convertToJSON(filename):
     data = {}
@@ -9,7 +10,7 @@ def convertToJSON(filename):
         csvReader = csv.reader(csvfile, delimiter = ',')
         for row in csvReader:
             data[row[1]] = {"category": row[0], "Ref - unopened": row[2], "Ref - opened": row[3], "Freezer": row[4], "Shelf": row[5]}
-            print(data[row[1]])
+
             
     with open(filename2, 'w') as jsonFile:
         jsonFile.write(json.dumps(data, indent = 4))
@@ -20,23 +21,18 @@ def expirationDate(url):
     item = URL[1][:-2]
     time = URL[2][:-2]
     storageMethod = URL[3][:-2]
-    opened = URL[4]
-
-    # lukes code for headers
-    # if storageMethod == "Refridgerator" and opened == True:
-    #     header = "Ref - Opened"
+    opened = bool(URL[4])
 
     if storageMethod == "Refridgerator" and opened == False:
         header = "Ref - unopened"
-
-    if storageMethod == "Refridgerator" and opened == True:
+    elif storageMethod == "Refridgerator" and opened == True:
         header = "Ref - opened"
-
-    if storageMethod == "Freezer":
+    elif storageMethod == "Freezer":
         header = "Freezer"
-
-    if storageMethod == "Shelf":
+    elif storageMethod == "Shelf":
         header = "Shelf"
+    else:
+        print("you are stupid")
     
 
     convertToJSON("BestBefore.csv")
@@ -44,24 +40,26 @@ def expirationDate(url):
     #lukes code for using JSON
     openJson = open('jsonfiletext.json')
     expiryDict = json.load(openJson)
-    expiryList = list(expiryDict.items())
+    expiryList = dict(expiryDict.items())
 
-    print(expiryList)
     for dictionary in expiryList:
-        print(dictionary)
         if dictionary == item:
-            print("bananavuifahbijkan")
             longevity = expiryList[item][header]
             category = expiryList[item]["category"]
 
-            year = time[0:4]
-            month = time[5:7]
-            date = time[8:10]
-            datePurchased = date(year,month,date)
+            year = int(time[0:4])
+            month = int(time[5:7])
+            date = int(time[8:10])
+            datePurchased = datetime.datetime(year,month,date)
 
             # creating an expiration date
-            expiryDate = datePurchased + timedelta(days = longevity)
+            if longevity == "NA":
+                return {"product": item, "time": time, "storage method": storageMethod, "expiry": "error in storage", "category": category, "opened": opened}
+            else:
+                expiryDate = datePurchased + timedelta(days = int(longevity))
+                print(expiryDate.year)
+                expiryDate = str(expiryDate.date())
 
     return {"product": item, "time": time, "storage method": storageMethod, "expiry": expiryDate, "category": category, "opened": opened}
 
-print(expirationDate("api/getExpiry?q=Buttermilk&t=2022-04-21&s=Refridgerator&o=opened"))
+print(expirationDate("api/getExpiry?q=Cheese, hard (such as cheddar, swiss, block parmesan)&t=2022-04-21&s=Refridgerator&o=True"))
