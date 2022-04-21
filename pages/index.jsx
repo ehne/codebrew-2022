d38bb0cdd429bc0d23019ffb0215ac3dbaa9ec95
Darcy LF <ehnevault@gmail.com>
2022-04-21 14:51:41 +1000

    :memo: add placeholder code to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Block } from 'baseui/block';
+import {ParagraphMedium} from 'baseui/typography'
 
 const Index = () => {
   return (
-    <div>
-      hi
-    </div>
+    <Block>
+      <ParagraphMedium>This is where the list of items would go</ParagraphMedium>
+    </Block>
   );
 }
 
