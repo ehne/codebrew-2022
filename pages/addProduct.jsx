ba9795a26365c235afa8dc13dddac22117e7e8a9
Darcy LF <ehnevault@gmail.com>
2022-04-21 16:26:29 +1000

    :sparkles: add tab selector on mode of entry

--- pages/addProduct.jsx
+++ pages/addProduct.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Block } from 'baseui/block';
 import { Manual } from '../components/addproduct';
+import {Tabs, Tab} from 'baseui/tabs-motion';
+
 
 const Addproduct = () => {
+  const [activeTabKey, setActiveTabKey] = React.useState('0');
+
   return (
     <Block>
-      <Manual />
+     <Tabs
+       onChange={({ activeKey }) => {
+         setActiveTabKey(activeKey);
+       }}
+       activeKey={activeTabKey}
+     >
+       <Tab title="Manual entry"><Manual /></Tab>
+       {/*<Tab title="Camera entry">Content 2</Tab>
+       <Tab title="List entry">Content 3</Tab> */}
+     </Tabs>
+      
     </Block>
   );
 }
