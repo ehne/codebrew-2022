e92b454883604c47758c640cf3d05043bb127deb
Darcy LF <ehnevault@gmail.com>
2022-04-21 14:53:25 +1000

    :lipstick: add css reset and viewport

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -19,6 +19,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <style>{`/*! minireset.css v0.0.6 | MIT License | github.com/jgthms/minireset.css */html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select{margin:0}html{box-sizing:border-box}*,*::before,*::after{box-sizing:inherit}img,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}`}</style>
           {this.props.stylesheets.map((sheet, i) => (
             <style
               className="_styletron_hydrate_"
@@ -28,6 +29,10 @@ class MyDocument extends Document {
               key={i}
             />
           ))}
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
         </Head>
         <body>
           <Main />
