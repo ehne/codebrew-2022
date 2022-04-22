56d45d136d567262b617f5f5f588851daf474dd9
Darcy LF <ehnevault@gmail.com>
2022-04-22 16:42:33 +1000

    :sparkles: add a more options page

--- pages/more/index.jsx
+++ pages/more/index.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import { Button } from 'baseui/button';
 import { useDB } from 'react-pouchdb';
+import {ListItem, ListItemLabel, ListHeading} from 'baseui/list';
+import { useRouter } from 'next/router';
+import { ParagraphSmall } from 'baseui/typography';
+import { Block } from 'baseui/block';
+import { styled } from 'baseui';
+import { ChevronRight } from 'baseui/icon';
+import {Drawer} from 'baseui/drawer';
 
+
+const CopyrightText = styled(ParagraphSmall, ({$theme})=>({
+  textAlign: 'center',
+  marginTop: $theme.sizing.scale1000,
+  color: $theme.colors.contentTertiary
+}))
 
 const Index = () => {
+  const [licenseIsOpen, setLicenseIsOpen] = React.useState(false);
   const db = useDB();
+  const router = useRouter();
+
   return (
-    <div>
-      <Button
-        onClick={() => {db.destroy()}}
+    <>
+      <ListHeading
+        heading="More options"
+        maxLines={1}
+      />
+      <ListItem endEnhancer={() => <Button
+        onClick={() => {
+          if(confirm('Are you sure you want to delete all data?')){
+            db.destroy();
+            router.push('/')
+          }
+        }}
         overrides={{
           BaseButton: {
             style: ({ $theme }) => ({
@@ -19,8 +44,22 @@ const Index = () => {
         }}
       >
         Delete Database
-      </Button>
-    </div>
+      </Button>}>
+        <ListItemLabel>Delete Dabase</ListItemLabel>
+      </ListItem>
+      <ListItem endEnhancer={() => <Button onClick={()=>setLicenseIsOpen(true)}><ChevronRight/></Button>}>
+        <ListItemLabel>View License Information</ListItemLabel>
+      </ListItem>
+      <CopyrightText>(c) 2022 TerrorBytes software</CopyrightText>
+
+      <Drawer
+        isOpen={licenseIsOpen}
+        autoFocus
+        onClose={() => setLicenseIsOpen(false)}
+      >
+        <div>drawer content</div>
+      </Drawer>
+    </>
   );
 }
 
