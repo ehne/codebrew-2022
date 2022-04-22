95608090780bcba1367ad4c91adbf639069df2e3
Darcy LF <ehnevault@gmail.com>
2022-04-22 16:15:00 +1000

    :sparkles: add delete database button to more page

--- pages/more/index.jsx
+++ pages/more/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Button } from 'baseui/button';
+import { useDB } from 'react-pouchdb';
+
 
 const Index = () => {
+  const db = useDB();
   return (
     <div>
-      
+      <Button
+        onClick={() => {db.destroy()}}
+        overrides={{
+          BaseButton: {
+            style: ({ $theme }) => ({
+              backgroundColor: $theme.colors.negative,
+              color: $theme.colors.contentOnColor
+            })
+          }
+        }}
+      >
+        Delete Database
+      </Button>
     </div>
   );
 }
