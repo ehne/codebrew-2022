2393339b30cea8427c3a16dbd1c4c1dd59fc0b3f
Darcy LF <ehnevault@gmail.com>
2022-04-22 14:41:47 +1000

    :sparkles add product card skeleton

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import {Card, StyledBody} from 'baseui/card';
+import {Skeleton} from 'baseui/skeleton';
+import {styled} from 'baseui';
 
-const ProductCard = () => {
+const MarginBottomCard = styled(Card, ({$theme}) => ({
+  marginBottom: $theme.sizing.scale500
+}))
+
+const ProductCard = ({skeleton=false, title}) => {
+  if (skeleton) {
+    return (
+      <MarginBottomCard
+        title={<Skeleton animation height="32px" width={`${150 + (Math.random() *200)}px`}/>}
+      >
+        <Skeleton
+         rows={2}
+         width="100%"
+         animation
+       />
+      </MarginBottomCard>
+    )
+  }
   return (
-    <div>
-      Card for product goes here
-    </div>
+   <MarginBottomCard title={title}>
+     <StyledBody>
+       Proin ut dui sed metus pharetra hend rerit vel non mi.
+       Nulla ornare faucibus ex, non facilisis nisl. Proin ut
+       dui sed metus pharetra hend rerit vel non mi. Nulla
+       ornare faucibus ex, non facilisis nisl.
+     </StyledBody>
+   </MarginBottomCard>
   );
 }
 
