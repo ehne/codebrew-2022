6a4744d3667626d2374026cd29ffa9cc9e8eb5d0
Darcy LF <ehnevault@gmail.com>
2022-04-22 15:23:39 +1000

    :sparkles: render products from db as cards

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { useDB } from 'react-pouchdb';
 import ProductCard from './ProductCard';
+import superjson from 'superjson';
 
 const ProductList = () => {
-  const [products, setProducts] = React.useState([''])
   const [cards, setCards] = React.useState(
-    <><ProductCard key="i" skeleton /> <ProductCard key="i" skeleton /></>
+    [<ProductCard key="a" skeleton />, <ProductCard key="b" skeleton />]
   )
   const db = useDB();
 
   React.useEffect(()=>{
-    db.allDocs().then(res => {
+    db.allDocs({include_docs: true}).then(res => {
       //console.log(res)
-      setCards(res.rows.map(i => <ProductCard key="i" title="hi"/>))
+      const renderedCards = res.rows.map(i => {
+        const loadedData = superjson.deserialize(i.doc)
+        console.log(loadedData)
+        return (
+          <ProductCard key={i.id} title={loadedData.productName} expiryDate={loadedData.expiryDate} addedDate={loadedData.addedDate}/>
+        )
+      })
+      console.log(renderedCards)
+      setCards(renderedCards)
     })
   }, [db])
   
-  return cards;
+  return <>{cards}</>;
 }
 
 export default ProductList;
