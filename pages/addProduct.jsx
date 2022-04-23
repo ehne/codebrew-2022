cf16cd090c38bd224f8dea7d253057385ab42d04
Darcy LF <ehnevault@gmail.com>
2022-04-23 11:23:03 +1000

    :sparkles: store selected input mode in query

--- pages/addProduct.jsx
+++ pages/addProduct.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { Block } from 'baseui/block';
 import { Manual, Smart } from '../components/addproduct';
 import {Tabs, Tab} from 'baseui/tabs-motion';
+import { useRouter } from 'next/router';
 
 
 const Addproduct = () => {
+  const router = useRouter();
   const [activeTabKey, setActiveTabKey] = React.useState('0');
+  
+  React.useEffect(() => {
+    setActiveTabKey(router.query.t)
+  }, [router]);
 
   return (
     <Block>
      <Tabs
        onChange={({ activeKey }) => {
          setActiveTabKey(activeKey);
+          router.push(`?t=${activeKey}`)
        }}
        activeKey={activeTabKey}
      >
