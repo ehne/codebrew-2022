ebce5d2d428b90a1b3c16e47eed02ec12395cb0a
Darcy LF <ehnevault@gmail.com>
2022-04-23 13:09:04 +1000

    :sparkles: get Smart entry to split between steps

--- components/addproduct/Smart.jsx
+++ components/addproduct/Smart.jsx
@@ -12,9 +12,11 @@ import ErrorState from '../ErrorState';
 import LoadingSpinner from '../LoadingSpinner';
 import productSchema from '../../lib/productSchema';
 import superjson from 'superjson';
+import { Block } from 'baseui/block';
 
 const Smart = () => {
   const { productList, isLoading, isError } = useProducts();
+  const [state, setState] = React.useState('beforeInput')
 
   const formik = useFormik({
     initialValues: { 
@@ -22,22 +24,39 @@ const Smart = () => {
       'expiryDate': new Date(), 
       'isOpened': false, 
       'storageLocation': 'Shelf', 
-      'purchasedDate': new Date() 
+      'addedDate': new Date() 
     },
     validationSchema: productSchema,
     onSubmit: (values, { setSubmitting }) => {
-      setTimeout(() => {
-        // TODO: clean up ID system for db items
-        db.put({
-          '_id': `${values.expiryDate.getTime()}.${uuid()}`,
-          ...superjson.serialize({
-            addedDate: (new Date()),
-            ...values,
-          }),
-        })
+      // if we're already showing suggestions, we can just then take 
+      // the submit handler to update the db
+      if (state === 'showSuggestions') {
+        setTimeout(() => {
+          // TODO: clean up ID system for db items
+          db.put({
+            '_id': `${values.expiryDate.getTime()}.${uuid()}`,
+            ...superjson.serialize({
+              addedDate: (new Date()),
+              ...values,
+            }),
+          })
+          setSubmitting(false);
+          router.push('/')
+        }, 400);
+      } else {
+        // find new suggestions
+        // convert time to format recognised by api YYYY-MM-DD
+        const dd = values.addedDate.getDate().toString().padStart(2, '0');
+        const mm = (values.addedDate.getMonth() + 1).toString().padStart(2, '0');
+        const yyyy = values.addedDate.getFullYear().toString().padStart(4, '0');
+        
+        const queries = `item=${encodeURIComponent(values.productName)}&storageMethod=${encodeURIComponent(values.storageLocation)}&opened=${values.isOpened}&time=${yyyy}-${mm}-${dd}`
+        console.log(queries)
+        //fetch(`/api/getExpiry2?${queries}`)
+
         setSubmitting(false);
-        router.push('/')
-      }, 400);
+        setState('showSuggestions')
+      }
     },
   });
 
@@ -53,9 +72,9 @@ const Smart = () => {
     });
   }, [productList, formik.values.productName, isLoading]);
 
-  if (isError) {
+  if (isError || state === 'error') {
     return (
-      <ErrorState href="/addproduct" />
+      <ErrorState href="/addProduct?t=1" />
     )
   }
 
@@ -74,6 +93,7 @@ const Smart = () => {
       >
         <Combobox
           name="productName"
+          disabled={formik.isSubmitting || state === 'showSuggestions'}
           value={formik.values.productName}
           onBlur={formik.handleBlur}
           onChange={value => formik.handleChange({ type: 'change', target: { type: 'text', value, name: 'productName' } })}
@@ -83,13 +103,14 @@ const Smart = () => {
         />
       </FormControl>
       <FormControl
-        label={() => "Expiry date"}
-        error={formik.errors.expiryDate}
+        label={() => "Date purchased"}
+        error={formik.errors.addedDate}
       >
         <DatePicker
-          name="expiryDate"
-          value={formik.values.expiryDate}
-          onChange={({ date }) => formik.handleChange({ type: 'change', target: { type: 'text', value: date, name: 'expiryDate' } })}
+          name="addedDate"
+          disabled={formik.isSubmitting || state === 'showSuggestions'}
+          value={formik.values.addedDate}
+          onChange={({ date }) => formik.handleChange({ type: 'change', target: { type: 'text', value: date, name: 'addedDate' } })}
           onBlur={formik.handleBlur}
         />
       </FormControl>
@@ -99,6 +120,7 @@ const Smart = () => {
       >
         <Combobox
           name="storageLocation"
+          disabled={formik.isSubmitting || state === 'showSuggestions'}
           value={formik.values.storageLocation}
           onBlur={formik.handleBlur}
           onChange={value => formik.handleChange({ type: 'change', target: { type: 'text', value, name: 'storageLocation' } })}
@@ -111,6 +133,42 @@ const Smart = () => {
           autocomplete
         />
       </FormControl>
+      <Button
+        endEnhancer={ChevronRight}
+        type="submit"
+        disabled={formik.isSubmitting || state === 'showSuggestions'}
+        isLoading={formik.isSubmitting}
+      >
+        Continue
+      </Button>
+      <Block marginTop="scale1000"/>
+      {
+        state === 'showSuggestions' ? (
+          <>
+            <FormControl
+              label={() => "Suggested expiry date"}
+              error={formik.errors.expiryDate}
+              caption={() => "From our database, this is the suggested expiry date. Feel free to change it."}
+            >
+              <DatePicker
+                name="expiryDate"
+                value={formik.values.expiryDate}
+                onChange={({ date }) => formik.handleChange({ type: 'change', target: { type: 'text', value: date, name: 'expiryDate' } })}
+                onBlur={formik.handleBlur}
+              />
+            </FormControl>
+            <Button
+              endEnhancer={ChevronRight}
+              type="submit"
+              disabled={formik.isSubmitting}
+              isLoading={formik.isSubmitting}
+            >
+              Submit
+            </Button>
+          </>
+        ) : ''
+      }
+      
     </form>
   );
 }
