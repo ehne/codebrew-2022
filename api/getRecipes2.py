1874d2210f9cc046f0531581ef227ec86675c8c1
Windchilled <lhastings@student.unimelb.edu.au>
2022-04-23 13:37:51 +1000

    Update getRecipes2.py

--- api/getRecipes2.py
+++ api/getRecipes2.py
@@ -3,35 +3,40 @@ import json
 import pip._vendor.requests as rq
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-'''
-url = "milk=eggs"
-URL = url.split("=")
-ingredients = []
-for p in URL:
-    ingredients.append(p)
 
-stringIngredients = (((str(ingredients).replace("'","")).replace("[","")).replace("]","")).replace(" ","")
-'''
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+
+        #Reading ingredients from URL Query
         s = self.path
         dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
+
+        ingredientNum = dic["ingNum"]
         ingredient1 = dic["ing1"]
         ingredient2 = dic["ing2"]
         ingredient3 = dic["ing3"]
 
-        stringIngredients = f"{ingredient1},{ingredient2},{ingredient3}"
+        #Determining how many ingredients need to be searched for
+        if ingredientNum == 1:
+            stringIngredients = ingredient1
+        if ingredientNum == 2:
+            stringIngredients = f"{ingredient1},{ingredient2}"
+        else:
+            stringIngredients = f"{ingredient1},{ingredient2},{ingredient3}"
+
         urlRequestGet = 'https://www.themealdb.com/api/json/v2/9973533/filter.php?i=' + stringIngredients
     
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
 
+        #Loading .json of recipes matching specified ingredients
         r = rq.get(urlRequestGet)
         recipes = r.json()
         listRecipes = list(recipes.items())[0][1]
 
+        #Creating list of recipe names, thumbnails, hyperlinks:
         if listRecipes != None:
             recipeInfoList = []
             for i in listRecipes:
@@ -45,5 +50,7 @@ class handler(BaseHTTPRequestHandler):
                 recipeInfo.append(linkRecipe)
                 recipeInfoList.append(recipeInfo)
             message = recipeInfoList
-        message = ["No recipes found with those ingredients."]
+        elif listRecipes == None:
+            message = ["No recipes found with those ingredients."]
+            
         return self.wfile.write(json.dumps(message).encode(encoding='utf-8'))
